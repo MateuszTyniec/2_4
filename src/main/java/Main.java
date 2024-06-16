bfee3d9c2e5c86567c005b91998663d910800745
MateuszTyniec <161605903+MateuszTyniec@users.noreply.github.com>
2024-06-16 20:16:04 +0000

    Commit2_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,10 @@
 
-import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Podaj 1 liczbę:");
-        int liczba1 = scanner.nextInt();
-
-        System.out.println("Podaj 2 liczbę:");
-        int liczba2 = scanner.nextInt();
-
-        System.out.println("Podaj 3 liczbę:");
-        int liczba3 = scanner.nextInt();
-
-        int najwieksza = liczba1;
-        if (liczba2 > najwieksza) {
-            najwieksza = liczba2;
-        }
-        if (liczba3 > najwieksza) {
-            najwieksza = liczba3;
+      public static void main(String[] args) {
+          String name = "Hello World";
+        for (int i = 0; i < name.length(); i++){
+          System.out.println(name.charAt(i));
         }
 
-        System.out.println("Największa liczba to: " + najwieksza);
-
-        scanner.close();
-    }
+        }
 }
