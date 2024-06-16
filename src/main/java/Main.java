0e261097fa931dd4f9d1b7036c79f6c77441ba5a
MateuszTyniec <161605903+MateuszTyniec@users.noreply.github.com>
2024-06-16 20:19:46 +0000

    Commit 2_3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,18 @@
 
-public class Main {
-      public static void main(String[] args) {
-          String name = "Hello World";
-        for (int i = 0; i < name.length(); i++){
-          System.out.println(name.charAt(i));
-        }
+  import java.util.Random;
 
-        }
-}
+  public class Main {
+      public static void main(String[] args) {
+          Random random = new Random();
+
+          int sumaKwadratow = 0;
+
+          for (int i = 0; i < 5; i++) {
+              int liczbaLosowa = random.nextInt(100); 
+              int kwadrat = liczbaLosowa * liczbaLosowa;
+              sumaKwadratow += kwadrat;
+              System.out.println( liczbaLosowa + " Kwadrat: " + kwadrat);
+          }
+          System.out.println("Suma kwadratów pięciu wylosowanych liczb: " + sumaKwadratow);
+      }
+  }
