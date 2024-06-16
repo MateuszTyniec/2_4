0331ccf9c97e0c5cb1ba0d0cf947c84126d8c4c4
MateuszTyniec <161605903+MateuszTyniec@users.noreply.github.com>
2024-06-16 20:23:27 +0000

    Commit 2_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,55 @@
+import java.util.Scanner;
 
-  import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-  public class Main {
-      public static void main(String[] args) {
-          Random random = new Random();
+        
+        System.out.println("Podaj pierwszą liczbę:");
+        double liczba1 = scanner.nextDouble();
+        System.out.println("Podaj drugą liczbę:");
+        double liczba2 = scanner.nextDouble();
 
-          int sumaKwadratow = 0;
+        System.out.println("Wybierz operację:");
+        System.out.println("1. Dodawanie");
+        System.out.println("2. Odejmowanie");
+        System.out.println("3. Mnożenie");
+        System.out.println("4. Dzielenie");
 
-          for (int i = 0; i < 5; i++) {
-              int liczbaLosowa = random.nextInt(100); 
-              int kwadrat = liczbaLosowa * liczbaLosowa;
-              sumaKwadratow += kwadrat;
-              System.out.println( liczbaLosowa + " Kwadrat: " + kwadrat);
-          }
-          System.out.println("Suma kwadratów pięciu wylosowanych liczb: " + sumaKwadratow);
-      }
-  }
+        int wybor = scanner.nextInt();
+
+        double wynik = 0;
+        switch (wybor) {
+            case 1:
+                wynik = liczba1 + liczba2;
+                break;
+            case 2:
+                wynik = liczba1 - liczba2;
+                break;
+            case 3:
+                wynik = liczba1 * liczba2;
+                break;
+            case 4:
+                if (liczba2 != 0) {
+                    wynik = liczba1 / liczba2;
+                } else {
+                    System.out.println("Nie można dzielić przez zero!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Błędny wybór!");
+                return;
+        }
+
+        System.out.println("Wynik: " + wynik);
+
+        scanner.close();
+    }
+}
+
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
